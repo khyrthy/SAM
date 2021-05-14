c4ccd2e85466846ed5a5dd18886662e77b6fbcc4
Nippyfus <76571984+Nippyfus@users.noreply.github.com>
2021-05-14 15:40:42 +0200

    Create install.py

---
+++ install.py
@@ -0,0 +1,28 @@
+import subprocess
+
+import unpkg
+
+from colorama import *
+def install(package_name):
+
+    package_source = None
+    packages_list = open("cache/MAIN.index","r")
+
+    for line in packages_list:
+        listed_package_name = line.split("=")[0]
+        listed_package_source = line.split("=")[1]
+
+        if package_name == listed_package_name:
+            package_source = listed_package_source
+
+    if package_source is None:
+        print(Fore.RED+"The specified package doesn't exists . Try running spm update"+Fore.RESET)
+        return 1
+
+    subprocess.call(["wget",package_source.rsplit()])
+    filename = package_source.rsplit().split("/")[-1]
+
+    err = unpkg.unpkg(filename)
+
+    if type(err) == int and err != 0:
+        return 1
